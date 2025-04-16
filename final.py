338fca79ca8024aa2a82a14f9fe738b5613405d3
JINGXIAN3 <126303338+JINGXIAN3@users.noreply.github.com>
2025-04-16 23:09:48 +0800

    Update final.py

--- final.py
+++ final.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from recommender_logic import content_df, content_features, language_decoder, director_decoder
+from recommender_logic import  content_features, language_decoder, director_decoder
 
 
 content_df = pd.read_csv("content_df.csv")
